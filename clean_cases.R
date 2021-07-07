1ee0bdf287ae67cc6374e04051e48e761aa1a194
aarcury-quandt <56981333+aarcury-quandt@users.noreply.github.com>
2021-07-07 13:15:34 -0400

    add incidence variable
    incidence variable added, case-count renamed cum_cases -> more descriptive, underscores play nicer with R

--- clean_cases.R
+++ clean_cases.R
@@ -2,13 +2,11 @@
 library(data.table)
 library(lubridate)
 
-.args <- if (interactive()) c(
-  "C:/Users/alice/Box/MMED/project/jhu-case_timeseries.csv", "C:/Users/alice/Box/MMED/project/jhu-case_timeseries_clean.rds"
-) else commandArgs(trailingOnly = TRUE)
-
-print(.args)
-
-res <- data.table()
+.debug <- "C:/Users/alice/Box/MMED/project"
+.args <- if (interactive()) sprintf(c(
+  "%s/jhu-case_timeseries.csv",
+  "%s/jhu-case_timeseries_clean.rds"
+), .debug[1]) else commandArgs(trailingOnly = TRUE)
 
 res <- fread(.args[1])
 
@@ -20,9 +18,13 @@ res <- res["Kenya"]
 res <- res[, `:=` (`Province/State` = NULL, Lat = NULL, Long = NULL)]
 
 #Long format
-res<- melt(res, id.vars = "Country/Region", variable.name = "date", value.name = "case-count")
+res<- melt(res, id.vars = "Country/Region", variable.name = "date", value.name = "cum_case")
 
 #reformating date from mdy to ymd
 res$date <- mdy(res$date)
+
+#creating incidence variable "new_case"
+res$new_case[1] <- 0
+res$new_case[2:nrow(res)] <- diff(res)
 
 saveRDS(res, tail(.args, 1))
