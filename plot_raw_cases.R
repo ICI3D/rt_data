4556b4e8325be353193eee1e12a7d84c9c15fdf0
Pragati <pragatiprasad@utexas.edu>
2021-07-08 08:48:06 -0400

    plotting files filled out

--- plot_raw_cases.R
+++ plot_raw_cases.R
@@ -3,13 +3,17 @@ library(lubridate)
 
 .debug <- "C:/Users/alice/Box/MMED/project"  ## remember to change this line to your local directory
 .args <- if (interactive()) sprintf(c(
-  "%s/jhu-case_timeseries_clean.rds",
+  "%s/rt_init.rds",
   "%s/fig_raw_cases.png"
 ), .debug[1]) else commandArgs(trailingOnly = TRUE)
 
 cleaned.data <- readRDS(.args[1])
 
 #' This script is to plot figures for incidence dynamics 
-#' 
-#' 
-#' 
+p <- ggplot(cleaned.data)+
+  geom_bar(aes(x = date, y = inc_case, stat = "identity"))+
+  ggtitle(label = "Kenya Raw Cases")
+
+png(tail(.args, 1))
+print(p)
+dev.off()
