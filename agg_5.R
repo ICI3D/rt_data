3045eb57d0979440287a9263cebc0243753e83ee
aarcury-quandt <56981333+aarcury-quandt@users.noreply.github.com>
2021-07-15 23:20:33 -0400

    add monthly and bi monthly aggregation schemes

--- agg_5.R
+++ agg_5.R
@@ -1,25 +1,25 @@
 library(data.table)
 library(lubridate)
 
-.debug <- "C:/Users/alice/Box/MMED/project"  ## remember to change this line to your local directory
+.debug <- "/Users/alicearcury-quandt/Documents/GitHub/rt_data"  ## remember to change this line to your local directory
 .args <- if (interactive()) sprintf(c(
   "%s/jhu-case_timeseries_clean.rds",
-  "%s/agg_4.rds"
+  "%s/agg_5.rds"
 ), .debug[1]) else commandArgs(trailingOnly = TRUE)
 
 cleaned.data <- readRDS(.args[1])
 
-#' reporting on first day of the month
+#' reporting on first or 15th day of the month
 #' 
 #' 
 
 clean <- cleaned.data[, agg := 0]
 
 clean[, md := mday(date) ]
-# roll to next 1st 
-clean[, firstgrp := cumsum(md == 2) ]
-clean[, agg := sum(new_case), by=firstgrp]
-clean[md != 1, agg := 0 ]
+# roll to next 1st or 15th
+clean[, bimonthgrp := cumsum(md == 2 | md == 16) ]
+clean[, agg := sum(new_case), by=bimonthgrp]
+clean[!(md %in% c(1,15)), agg := 0 ]
 
 res <- clean[, .(date, new_case = agg)]
 
